dafa0b27e373f7164a9b86e4c6e5df073ef4fe6f
Linus Gasser <linus@gasser.blue>
2020-01-04 07:41:36 +0100

    removing memoryLane adding links to nodes

--- cymidb/db_test.go
+++ cymidb/db_test.go
@@ -40,13 +40,45 @@ func TestDB_GetLatest(t *testing.T) {
 	defer os.Remove(f.Name())
 	db, err := CreateDBFile(f.Name(), "tmp", "http://")
 	require.NoError(t, err)
+
 	db.Device.Name = "temp"
-	db.Device.UpdateNode()
-	require.NoError(t, db.UpdateNode(db.Device.Node))
+	require.NoError(t, db.SaveNode(db.Device))
 	db.Close()
 
 	db, err = OpenDBFile(f.Name())
 	require.NoError(t, err)
 	require.Equal(t, "temp", db.Device.Name)
 	db.Close()
+}
+
+func TestDB_Links(t *testing.T) {
+	db, err := CreateDBFile(":memory:", "tmp1", "")
+	require.NoError(t, err)
+	defer db.Close()
+
+	td := NewDataType("blue.gasser/test/blob")
+
+	n1 := NewNode(NodeBlob, Data{td, []byte("blob1")})
+	n2 := NewNode(NodeBlob, Data{td, []byte("blob2")})
+	require.NoError(t, db.SaveNode(n1))
+	require.NoError(t, db.SaveNode(n2))
+	require.NoError(t, db.AddLink(n1.NodeID, n2.NodeID))
+
+	children, err := db.GetChildrenNodes(n1.NodeID)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(children))
+	require.NoError(t, n2.Equals(children[0]))
+
+	children, err = db.GetChildrenNodes(n2.NodeID)
+	require.NoError(t, err)
+	require.Equal(t, 0, len(children))
+
+	ancestors, err := db.GetAncestorsNodes(n2.NodeID)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(ancestors))
+	require.NoError(t, n1.Equals(ancestors[0]))
+
+	ancestors, err = db.GetAncestorsNodes(n1.NodeID)
+	require.NoError(t, err)
+	require.Equal(t, 0, len(ancestors))
 }
--- cymidb/device.go
+++ cymidb/device.go
@@ -3,14 +3,13 @@ package cymidb
 import (
 	"errors"
 	"fmt"
-	"time"
 )
 
 // Device holds all information for a new device.
 type Device struct {
-	Node
 	Name string
 	URL  string
+	node Node
 }
 
 // DataTypeDeviceName is used in a device node to represent the name of the device.
@@ -25,7 +24,7 @@ func NewDeviceFromNode(n Node) (dev Device, err error) {
 	if n.Type != NodeDev {
 		return dev, errors.New("node is not of type device")
 	}
-	dev.Node = n
+	dev.node = n
 	md, err := n.GetData(DataTypeDeviceName)
 	if err != nil {
 		return dev, fmt.Errorf("couldn't get data: %+v", err)
@@ -36,21 +35,18 @@ func NewDeviceFromNode(n Node) (dev Device, err error) {
 
 // CreateNewNode returns a node that
 func NewDevice(name string) (dev Device) {
-	dev.Node = Node{
-		ID:      RandomNodeID(),
-		Type:    NodeDev,
-		Date:    time.Now().Unix(),
-		Version: 0,
-	}
+	dev.node = NewNode(NodeDev)
 	dev.Name = name
-	dev.UpdateNode()
 	return
 }
 
-// UpdateNode copies the name and url to the node structure.
-func (dev *Device) UpdateNode() {
-	dev.Node.Datas = []Data{
-		{Type: DataTypeDeviceName, Data: []byte(dev.Name)},
-		{Type: DataTypeDeviceURL, Data: []byte(dev.URL)},
-	}
+// GetNode makes sure that the dataBuf of the node is updated and returns the updated node.
+func (dev Device) GetNode() (Node, error) {
+	return dev.node, dev.updateNode()
+}
+
+// updateNode copies the name and url to the node structure.
+func (dev *Device) updateNode() error {
+	return dev.node.SetDatas(Data{Type: DataTypeDeviceName, Data: []byte(dev.Name)},
+		Data{Type: DataTypeDeviceURL, Data: []byte(dev.URL)})
 }
